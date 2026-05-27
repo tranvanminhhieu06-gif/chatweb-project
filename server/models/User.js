9809fe8305f53229e889efdf20994515c61cf168
TranMinhHieu <tranvanminhhieu06@gmail.com>
2026-05-28 06:24:03 +0700

    Style: Add style.css with custom scrollbar and glow animations

--- server/models/User.js
+++ server/models/User.js
@@ -9,15 +9,10 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Tự động mã hóa mật khẩu trước khi lưu vào database
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method hỗ trợ kiểm tra mật khẩu khi đăng nhập
